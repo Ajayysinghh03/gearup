a8d60f6bc9326f70e59e8a3b71072cd8ee1f712e
Ajay Singh Thakur <ajayysinghh2004@gmail.com>
2025-07-06 10:46:08 +0530

    deployment ready

--- gearupmain/settings.py
+++ gearupmain/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-q0ae8b!-r3@v^qjykie3)4n6--dop6n7nms99dmni8hm^1x^rj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
